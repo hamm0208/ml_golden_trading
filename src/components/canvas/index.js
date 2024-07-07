12f34864b026fe64c04d12c40c15c6def99f907b
hamm0208 <107385845+hamm0208@users.noreply.github.com>
2024-07-07 20:17:11 +0800

    Components
    Created the components for the landing page

---
+++ src/components/canvas/index.js
@@ -0,0 +1,3 @@
+import Product1Canvas from "./product1";
+
+export {Product1Canvas};
---
+++ src/components/index.js
@@ -0,0 +1,21 @@
+import {Product1Canvas} from "./canvas"
+import AboutUs from "./AboutUs"
+import Benefits from "./Benefits"
+import Footer from "./Footer"
+import Hero from "./Hero"
+import Navbar from "./Navbar"
+import Order from "./Order"
+import Procedure from "./Procedure"
+import WhyUs from "./WhyUs"
+
+export {
+    Product1Canvas,
+    AboutUs,
+    Benefits,
+    Footer,
+    Hero,
+    Navbar,
+    Order,
+    Procedure,
+    WhyUs
+}
